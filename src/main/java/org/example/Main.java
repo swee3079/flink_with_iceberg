201fb337e43ed8631b427589f1c56b9a6d14caec
Sisuka Weerasinghe <sisuka.weerasinghe@sysco.com>
2025-07-18 09:39:28 +0530

    locally setupped flink

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
+        executionEnvironment.execute();
     }
 }
